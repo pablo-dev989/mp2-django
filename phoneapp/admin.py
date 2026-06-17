e5fc41ae9d6b09d8d3c616937ae90a39aa771fab
pablo-dev989 <261613295+pablo-dev989@users.noreply.github.com>
2026-06-17 18:15:58 -0400

    Se agregan las credenciales del usuario admin de django

--- phoneapp/admin.py
+++ phoneapp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from phoneapp.models import Manufacturer, Smartphone
 
+### usuario  : simon
+### password : simon
+
 # Register your models here.
 admin.site.register(Manufacturer)
 admin.site.register(Smartphone)
